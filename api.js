f2656f06344cd15f2f767c8897ff219edbbc2c01
LouisCu <lcuvelier33@gmail.com>
2018-02-13 15:34:13 +0100

    Feature : Ajout inquirer

--- api.js
+++ api.js
@@ -2,8 +2,10 @@ const Client = require('node-rest-client').Client;
 
 const client = new Client();
 
-client.registerMethod("bitcoin", "https://api.coinmarketcap.com/v1/ticker/bitcoin", "GET");
-
-client.methods.bitcoin(function (data) {
+const callApi = (currency) => {
+  client.get(`https://api.coinmarketcap.com/v1/ticker/${currency}`, function (data) {
     console.log(data);
-});
+  });
+};
+
+module.exports = callApi;
---
+++ inquirer.js
@@ -0,0 +1,26 @@
+const inquirer = require('inquirer');
+const callApi = require('./api.js');
+
+inquirer.prompt([
+  {
+    type: 'checkbox',
+    message: 'Which crypto-currency do you want to track ?',
+    name: 'cryptoChoices',
+    choices: [
+      'Bitcoin',
+      'Ethereum',
+      'Monero',
+      'Stellar',
+      'Litecoin',
+    ]
+  }
+]).then((answers) => {
+  const cryptoChoices = answers.cryptoChoices;
+
+  cryptoChoices.map((currency) => {
+    console.log(currency);
+    callApi(currency.toLowerCase());
+  });
+}).catch((err) => {
+  console.log(err);
+});
