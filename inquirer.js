c6845e17d0650407d5adbaa8cabbad7ed17af8d0
LouisCu <lcuvelier33@gmail.com>
2018-02-20 12:36:29 +0100

    Update : Changement de package pour la search dans la checkbox

--- inquirer.js
+++ inquirer.js
@@ -1,45 +1,22 @@
 const inquirer = require("inquirer");
 const { getCurrencies, showCurrencies} = require("./api.js");
 
-const filterCurrencies = async (currencies, answers, input) => {
-  input = input || '';
-  const inputArray = input.split(' ');
-
-  try {
-    return await currencies.filter((currency) => {
-      let shouldInclude = true;
-
-      inputArray.forEach((inputChunk) => {
-        if (!currency.toLowerCase().includes(inputChunk.toLowerCase())) {
-          shouldInclude = false
-        }
-      });
-      return shouldInclude;
-    });
-  } catch(err) {
-    console.log(err);
-  }
-};
-
 const init = async () => {
   try {
     console.log("Loading currencies ...");
     const currencies = await getCurrencies();
-    await inquirer.registerPrompt('checkbox-search', require('inquirer-checkbox-search'));
+    await inquirer.registerPrompt("search-checkbox", require("inquirer-search-checkbox"));
     const answers = await inquirer.prompt([
       {
-        type: "checkbox-search",
+        type: "search-checkbox",
         message: "Which crypto-currency do you want to track ?",
         name: "cryptoChoices",
-        source: (answers, input) => {
-          return filterCurrencies(currencies.names, answers, input)
-        }
+        choices: currencies.names,
       }
     ]);
 
     const cryptoChoices = answers.cryptoChoices;
     showCurrencies(cryptoChoices);
-
   } catch (err) {
     console.log(err);
   }
