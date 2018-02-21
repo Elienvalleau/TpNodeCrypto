99e31fb42c1d5cc1bab820686d9daabd3ed4a68a
LouisCu <lcuvelier33@gmail.com>
2018-02-21 10:54:55 +0100

    Fix : Sequelize type error

--- db.js
+++ db.js
@@ -1,8 +1,13 @@
-const Sequelize = require('sequelize');
-const db = new Sequelize('database', 'username', 'password', {
-    host: 'localhost',
-    dialect: 'sqlite',
-    storage: './database.sqlite',
-    logging: false
+const Sequelize = require("sequelize");
+const Op = Sequelize.Op;
+const db = new Sequelize("database", "username", "password", {
+  host: "localhost",
+  dialect: "sqlite",
+  storage: "./database.sqlite",
+  logging: false,
+  operatorsAliases: {
+    $eq: Op.eq,
+    $like: Op.like
+  }
 });
 module.exports = db;
